988e3248dbf1bfcc51e096c152589fd9079d1fe5
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-12 22:19:09 +0200

    Implement i18n helpers for crud actions.
      def t_attr(attribute, model = nil)
      def t_model(model = nil)
      def t_crud(action = nil, model = nil)
      def t_confirm_delete(record)

--- init.rb
+++ init.rb
@@ -1,1 +1,2 @@
 # Include hook code here
+ActionController::Base.helper I18nRailsHelpers
--- lib/i18n_rails_helpers.rb
+++ lib/i18n_rails_helpers.rb
@@ -1,1 +1,35 @@
-# I18nRailsHelpers
+module I18nRailsHelpers
+  def t_attr(attribute, model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    t(attribute, :scope => [:activerecord, :attributes, model_name])
+  end
+
+  def t_model(model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    t(model_name, :scope => [:activerecord, :models])
+  end
+
+  def t_crud(action = nil, model = nil)
+    if model.is_a? Class
+      model_name = model.name.underscore
+    elsif model.nil?
+      model_name = controller_name.singularize
+    end
+    
+    action ||= action_name
+    t(action, :scope => :crud, :model => model_name.capitalize)
+  end
+  
+  def t_confirm_delete(record)
+    t('messages.confirm_delete', :record => "#{t_model(record.class)} #{record.to_s}")
+  end
+end
+
