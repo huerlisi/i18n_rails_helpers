0914756d1aee8c6838556d5be0fe18f5a3dea7ce
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-12 23:18:06 +0200

    Add rdoc api documentation.

--- lib/i18n_rails_helpers.rb
+++ lib/i18n_rails_helpers.rb
@@ -1,4 +1,13 @@
 module I18nRailsHelpers
+  # Returns translated name for the given +attribute+.
+  #
+  # If no +model+ is given, it uses the controller name to guess the model by
+  # singularize it.
+  #
+  # Example:
+  #   t_attr('first_name', Patient) => 'Vorname'
+  #   t_attr('first_name')          => 'Vorname' # when called in patients_controller views
+  #
   def t_attr(attribute, model = nil)
     if model.is_a? Class
       model_name = model.name.underscore
@@ -8,6 +17,16 @@ module I18nRailsHelpers
     I18n::translate(attribute, :scope => [:activerecord, :attributes, model_name])
   end
 
+  # Returns translated name for the given +model+.
+  #
+  # If no +model+ is given, it uses the controller name to guess the model by
+  # singularize it. +model+ can be both a class or an actual instance.
+  #
+  # Example:
+  #   t_model(Account)     => 'Konto'
+  #   t_model(Account.new) => 'Konto'
+  #   t_model              => 'Konto' # when called in patients_controller views
+  #
   def t_model(model = nil)
     if model.is_a? Class
       model_name = model.name.underscore
@@ -19,6 +38,18 @@ module I18nRailsHelpers
     I18n::translate(model_name, :scope => [:activerecord, :models])
   end
 
+  # Returns translated title for current +action+ on +model+.
+  #
+  # If no +action+ is given, it uses the current action.
+  #
+  # If no +model+ is given, it uses the controller name to guess the model by
+  # singularize it. +model+ can be both a class or an actual instance.
+  #
+  # Example:
+  #   t_crud('new', Account') => 'Konto anlegen'
+  #   t_crud('delete')        => 'Konto löschen' # when called in accounts_controller views
+  #   t_crud                  => 'Konto ändern'  # when called in accounts_controller edit view
+  #
   def t_crud(action = nil, model = nil)
     if model.is_a? Class
       model_name = model.name.underscore
@@ -30,6 +61,13 @@ module I18nRailsHelpers
     I18n::translate(action, :scope => :crud, :model => model_name.capitalize)
   end
   
+  # Returns translated deletion confirmation for +record+.
+  #
+  # It uses +record+.to_s in the message.
+  #
+  # Example:
+  #   t_confirm_delete(@account) => 'Konto Kasse wirklich löschen'
+  #
   def t_confirm_delete(record)
     I18n::translate('messages.confirm_delete', :record => "#{t_model(record.class)} #{record.to_s}")
   end
