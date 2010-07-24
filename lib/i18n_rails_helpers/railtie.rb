8680a4793682acc44efd76376a7fda4800abc9c1
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-07-25 00:11:49 +0200

    Use an Engine, not just Railtie to load the locales.

--- lib/i18n_rails_helpers/railtie.rb
+++ lib/i18n_rails_helpers/railtie.rb
@@ -2,7 +2,7 @@ require 'i18n_rails_helpers'
 require 'rails'
 
 module I18nRailsHelpers
-  class Railtie < Rails::Railtie
+  class Railtie < Rails::Engine
     initializer :after_initialize do
       ActionController::Base.helper I18nRailsHelpers
     end
