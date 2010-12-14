33f7233836f6f2e3db67e8acecc4ffeff0382b7f
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-14 11:10:17 +0100

    Add contextual link helpers.

--- lib/i18n_rails_helpers/railtie.rb
+++ lib/i18n_rails_helpers/railtie.rb
@@ -1,10 +1,12 @@
 require 'i18n_rails_helpers'
+require 'contextual_link_helpers'
 require 'rails'
 
 module I18nRailsHelpers
   class Railtie < Rails::Engine
     initializer :after_initialize do
       ActionController::Base.helper I18nRailsHelpers
+      ActionController::Base.helper ContextualLinkHelpers
     end
   end
 end
