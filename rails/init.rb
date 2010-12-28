4c53598a8dea0ad9977ae1074a56099f364069bd
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2010-12-28 14:03:03 +0100

    Make contexual link helpers available in rails < 3.

--- rails/init.rb
+++ rails/init.rb
@@ -1,3 +1,5 @@
 # Register helpers for Rails < 3
 require File.join(File.dirname(__FILE__), *%w[.. lib i18n_rails_helpers])
+require File.join(File.dirname(__FILE__), *%w[.. lib contextual_link_helpers])
 ActionView::Base.send :include, I18nRailsHelpers
+ActionView::Base.send :include, ContextualLinkHelpers
