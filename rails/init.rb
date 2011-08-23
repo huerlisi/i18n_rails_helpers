9c1c1a7e94557cd1828b2ed2b3efa6674f93651a
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2011-08-23 10:54:25 +0200

    Include list link helpers for Rails < 3.

--- rails/init.rb
+++ rails/init.rb
@@ -1,5 +1,7 @@
 # Register helpers for Rails < 3
 require File.join(File.dirname(__FILE__), *%w[.. lib i18n_rails_helpers])
-require File.join(File.dirname(__FILE__), *%w[.. lib contextual_link_helpers])
 ActionView::Base.send :include, I18nRailsHelpers
+require File.join(File.dirname(__FILE__), *%w[.. lib contextual_link_helpers])
 ActionView::Base.send :include, ContextualLinkHelpers
+require File.join(File.dirname(__FILE__), *%w[.. lib list_link_helpers.rb])
+ActionView::Base.send :include, ListLinkHelpers
