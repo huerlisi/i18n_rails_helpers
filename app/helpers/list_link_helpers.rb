a5a72c7e412459ed38cc898eddeef2c13ef6fc84
Simon Hürlimann <simon.huerlimann@cyt.ch>
2014-04-08 07:46:56 +0200

    Whitespace cleanup.

--- app/helpers/list_link_helpers.rb
+++ app/helpers/list_link_helpers.rb
@@ -5,11 +5,11 @@ module ListLinkHelpers
     
     link_to(t_action(action), url, options)
   end
-  
+
   def list_link_for(action, resource_or_model = nil, options = {})
     # Handle both symbols and strings
     action = action.to_s
-    
+
     # Resource and Model setup
     # Support nested resources
     if resource_or_model.is_a? Array
@@ -17,7 +17,7 @@ module ListLinkHelpers
     else
       main_resource_or_model = resource_or_model
     end
-    
+
     # Use controller name to guess resource or model if not specified
     case action
     when 'new', 'index'
@@ -27,10 +27,10 @@ module ListLinkHelpers
       model = resource.class
     end
     model_name = model.to_s.underscore
-    
+
     # No link if CanCan is used and current user isn't authorized to call this action
     return if respond_to?(:can?) and cannot?(action.to_sym, model)
-    
+
     # Link generation
     case action
     when 'index', 'show'
@@ -41,17 +41,17 @@ module ListLinkHelpers
     else
       path = polymorphic_path(resource_or_model, :action => action)
     end
-    
+
     return list_link_to(action, path, options)
   end
-  
+
   def list_links_for(action = nil, resource_or_model = nil)
     # Use current action if not specified
     action ||= action_name
-    
+
     # Handle both symbols and strings
     action = action.to_s
-    
+
     actions = []
     case action
     when 'new', 'create'
@@ -63,9 +63,9 @@ module ListLinkHelpers
     when 'index'
       actions << 'new'
     end
-    
+
     links = actions.map{|link_for| contextual_link_to(link_for, resource_or_model)}
-    
+
     return links.join("\n").html_safe
   end
 end
