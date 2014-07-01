2d0d1bd7e167d2075ab70046ae57789b35b9b533
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2014-07-01 06:55:41 +0200

    Drop has_vcards code from spec/dummy.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-
-  mount HasVcards::Engine => "/has_vcards"
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,52 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140620151641) do
 
-  create_table "has_vcards_addresses", force: true do |t|
-    t.string   "post_office_box",  limit: 50
-    t.string   "extended_address", limit: 50
-    t.string   "street_address",   limit: 50
-    t.string   "locality",         limit: 50
-    t.string   "region",           limit: 50
-    t.string   "postal_code",      limit: 50
-    t.string   "country_name",     limit: 50
-    t.integer  "vcard_id"
-    t.string   "address_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "has_vcards_addresses", ["vcard_id"], name: "addresses_vcard_id_index", using: :btree
-
-  create_table "has_vcards_phone_numbers", force: true do |t|
-    t.string   "number",            limit: 50
-    t.string   "phone_number_type", limit: 50
-    t.integer  "vcard_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "has_vcards_phone_numbers", ["phone_number_type"], name: "index_has_vcards_phone_numbers_on_phone_number_type", using: :btree
-  add_index "has_vcards_phone_numbers", ["vcard_id"], name: "phone_numbers_vcard_id_index", using: :btree
-
-  create_table "has_vcards_vcards", force: true do |t|
-    t.string   "full_name",        limit: 50
-    t.string   "nickname",         limit: 50
-    t.string   "family_name",      limit: 50
-    t.string   "given_name",       limit: 50
-    t.string   "additional_name",  limit: 50
-    t.string   "honorific_prefix", limit: 50
-    t.string   "honorific_suffix", limit: 50
-    t.boolean  "active",                      default: true
-    t.string   "type"
-    t.integer  "reference_id"
-    t.string   "reference_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "has_vcards_vcards", ["active"], name: "index_has_vcards_vcards_on_active", using: :btree
-  add_index "has_vcards_vcards", ["reference_id", "reference_type"], name: "index_has_vcards_vcards_on_reference_id_and_reference_type", using: :btree
-
   create_table "somethings", force: true do |t|
     t.string "title"
   end
