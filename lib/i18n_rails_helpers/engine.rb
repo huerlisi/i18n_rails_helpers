cf31c24d0293011bb052efba36c1b06e9f353080
Kaspar Vollenweider <vok@panter.ch>
2019-05-05 13:03:12 +0200

    remove contextual link helpers

--- lib/i18n_rails_helpers/engine.rb
+++ lib/i18n_rails_helpers/engine.rb
@@ -6,8 +6,6 @@ module I18nRailsHelpers
   class Engine < Rails::Engine
     initializer 'i18n_rails_helpers.helper' do
       ActionView::Base.send :include, I18nHelpers
-      ActionView::Base.send :include, ContextualLinkHelpers
-      ActionView::Base.send :include, ListLinkHelpers
       ActionController::Base.class_eval { include ControllerHelpers }
 
       ActiveRecord::Base.class_eval do
--- spec/helpers/list_link_helpers_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-describe ListLinkHelpers do
-  describe "#list_link_to" do
-    it "adds class options" do
-      expect(helper.list_link_to(:show, '/dummy', class: 'test')).to have_css('a.test')
-    end
-  end
-end
