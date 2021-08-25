927c5a38c2a950b554392179b4c17abe97d400b5
Crispin Müller <crispin.mueller@9elements.com>
2021-08-25 22:15:01 +0200

    fix indent

--- lib/i18n_rails_helpers/engine.rb
+++ lib/i18n_rails_helpers/engine.rb
@@ -6,11 +6,11 @@ module I18nRailsHelpers
   class Engine < Rails::Engine
     initializer 'i18n_rails_helpers.helper' do
       Rails.application.reloader.to_prepare do
-          ActionView::Base.send :include, I18nHelpers
-          ActionView::Base.send :include, ContextualLinkHelpers
-          ActionView::Base.send :include, ListLinkHelpers
-        end
-        ActionController::Base.class_eval { include ControllerHelpers }
+        ActionView::Base.send :include, I18nHelpers
+        ActionView::Base.send :include, ContextualLinkHelpers
+        ActionView::Base.send :include, ListLinkHelpers
+      end
+      ActionController::Base.class_eval { include ControllerHelpers }
 
       ActiveRecord::Base.class_eval do
         include ModelHelpers
