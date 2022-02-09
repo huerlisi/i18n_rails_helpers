e76b70058ecc8c77a987b32492a236c73a3fb635
Kaspar Vollenweider <vok@panter.ch>
2022-02-09 19:20:35 +0100

    upgrade ruby do 2.7 and rails >= 6.0.0

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 require 'rspec/rails'
-require 'factory_girl_rails'
+require 'factory_bot_rails'
 
 Rails.backtrace_cleaner.remove_silencers!
 
